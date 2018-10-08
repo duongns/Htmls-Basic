717b7fad15ab99ea62da38432a96ba6bdccfc49c
Nguyễn Sơn Dương <duongns84@gmail.com>
2018-10-08 20:18:10 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,35 @@ $(window).load(function() {
 });
 
 jQuery(document).ready(function($) {
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('header').outerHeight();
+	$(window).scroll(function(event) {
+	    "use strict";
+	    didScroll = true;
+	});
+	setInterval(function() {
+	    "use strict";
+	    if (didScroll) {
+		hasScrolled();
+		didScroll = false;
+	    }
+	}, 150);
+	function hasScrolled() {
+	    var st = $(this).scrollTop();
+	    if (Math.abs(lastScrollTop - st) <= delta)
+		return;
+	    if (st > lastScrollTop && st > navbarHeight) {
+		$('header').removeClass('nav-down').addClass('nav-up');
+	    } else {
+		if (st + $(window).height() < $(document).height()) {
+		    $('header').removeClass('nav-up').addClass('nav-down');
+		}
+	    }
+	    lastScrollTop = st;
+	}
+	
 // Tabbed
     $('.tabbed').each(function(){
        $(function(){
@@ -34,7 +63,7 @@ jQuery(document).ready(function($) {
         $(this).prev('input').val('').focus();
     }));
 
-// Back top top
+// Back to top
 	$('a.back-to-top').click(function() {
 		$('html, body').animate({
 			scrollTop: 0
